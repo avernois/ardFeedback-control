4980511a59c7c04d4362a22a59322824859bbd88
Antoine <antoine@crafting-labs.fr>
2012-08-13 18:39:06 +0200

    refactoring of test
    add a method to generate jenkins' xml with parameterized statuses.

--- test/ard_feedback_test.rb
+++ test/ard_feedback_test.rb
@@ -4,137 +4,10 @@ require_relative "../lib/ard_feedback.rb"
 
 class ArdFeedbackTest < Test::Unit::TestCase
 
-  def setup
-
-    @blue_xml = <<EOL
-    <hudson>
-    <job>
-    <name>thats_my_job</name>
-    <url>http://localhost:8080/job/thats_my_job/</url>
-    <color>blue</color>
-    </job>
-    <job>
-    <name>thats_my_job</name>
-    <url>http://localhost:8080/job/thats_my_job/</url>
-    <color>blue</color>
-    </job>
-
-    </hudson>
-EOL
-
-    @yellow_xml = <<EOL
-    <hudson>
-    <job>
-    <name>thats_my_job</name>
-    <url>http://localhost:8080/job/thats_my_job/</url>
-    <color>yellow</color>
-    </job>
-    <job>
-    <name>thats_my_job</name>
-    <url>http://localhost:8080/job/thats_my_job/</url>
-    <color>blue</color>
-    </job>
-
-    </hudson>
-EOL
-
-
-    @blue_and_anime_xml = <<EOL
-    <hudson>
-    <job>
-    <name>thats_my_job</name>
-    <url>http://localhost:8080/job/thats_my_job/</url>
-    <color>blue</color>
-    </job>
-    <job>
-    <name>thats_my_job</name>
-    <url>http://localhost:8080/job/thats_my_job/</url>
-    <color>blue_anime</color>
-    </job>
-    </hudson>
-EOL
-
-    @yellow_and_anime_xml = <<EOL
-    <hudson>
-    <job>
-    <name>thats_my_job</name>
-    <url>http://localhost:8080/job/thats_my_job/</url>
-    <color>yellow</color>
-    </job>
-    <job>
-    <name>thats_my_job</name>
-    <url>http://localhost:8080/job/thats_my_job/</url>
-    <color>yellow_anime</color>
-    </job>
-    </hudson>
-EOL
-
-    @blue_and_yellow_xml = <<EOL
-    <hudson>
-    <job>
-    <name>thats_my_job</name>
-    <url>http://localhost:8080/job/thats_my_job/</url>
-    <color>blue</color>
-    </job>
-    <job>
-    <name>thats_my_job</name>
-    <url>http://localhost:8080/job/thats_my_job/</url>
-    <color>yellow</color>
-    </job>
-    </hudson>
-EOL
-
-    @blue_and_red_xml = <<EOL
-    <hudson>
-    <job>
-    <name>thats_my_job</name>
-    <url>http://localhost:8080/job/thats_my_job/</url>
-    <color>blue</color>
-    </job>
-    <job>
-    <name>thats_my_job</name>
-    <url>http://localhost:8080/job/thats_my_job/</url>
-    <color>red</color>
-    </job>
-    </hudson>
-EOL
-
-    @yellow_and_red_xml = <<EOL
-    <hudson>
-    <job>
-    <name>thats_my_job</name>
-    <url>http://localhost:8080/job/thats_my_job/</url>
-    <color>yellow</color>
-    </job>
-    <job>
-    <name>thats_my_job</name>
-    <url>http://localhost:8080/job/thats_my_job/</url>
-    <color>red</color>
-    </job>
-    </hudson>
-EOL
-
-
-    @red_and_blue_anime_xml = <<EOL
-    <hudson>
-    <job>
-    <name>thats_my_job</name>
-    <url>http://localhost:8080/job/thats_my_job/</url>
-    <color>red</color>
-    </job>
-    <job>
-    <name>thats_my_job</name>
-    <url>http://localhost:8080/job/thats_my_job/</url>
-    <color>blue_anime</color>
-    </job>
-    </hudson>
-EOL
-
-    @serial = SerialMock.new
-    @ardFeedback = ArdFeedback.new @serial
-
+    def setup 
+        @serial = SerialMock.new
+        @ardFeedback = ArdFeedback.new @serial
     end
-
 
     def test_success_should_blink_led
         @ardFeedback.light_led :building
@@ -155,53 +28,67 @@ EOL
         @ardFeedback.light_led :unstable
         assert_equal("Y", @serial.last_written)
     end
-    
+
     def test_blue_only_builds_is_success_build
-        status = @ardFeedback.get_status @blue_xml
+        status = @ardFeedback.get_status xml_jenkins ["blue"]
 
         assert_equal(:success, status)
     end
 
     def test_yellow_only_builds_is_unstable_build
-        status = @ardFeedback.get_status @yellow_xml
+        status = @ardFeedback.get_status xml_jenkins ["yellow"]
 
         assert_equal(:unstable, status)
     end
 
     def test_blue_and_yellow_is_unstable_build
-        status = @ardFeedback.get_status @blue_and_yellow_xml
+        status = @ardFeedback.get_status xml_jenkins ["blue", "yellow"]
 
         assert_equal(:unstable, status)
     end
 
     def test_yellow_and_anime_is_building
-        status = @ardFeedback.get_status @yellow_and_anime_xml
+        status = @ardFeedback.get_status xml_jenkins ["yellow", "blue_anime"]
 
         assert_equal(:building, status)
     end
 
     def test_blue_and_red_builds_is_failed_build
-        status = @ardFeedback.get_status @blue_and_red_xml
+        status = @ardFeedback.get_status xml_jenkins ["blue", "red"]
 
         assert_equal(:failed, status)
     end
 
     def test_yellow_and_red_builds_is_failed_build
-        status = @ardFeedback.get_status @yellow_and_red_xml
+        status = @ardFeedback.get_status xml_jenkins ["yellow", "red"]
 
         assert_equal(:failed, status)
     end
 
     def test_blue_and_anime_builds_is_building
-        status = @ardFeedback.get_status @blue_and_anime_xml
+        status = @ardFeedback.get_status xml_jenkins ["blue", "blue_anime"]
 
         assert_equal(:building, status)
     end
 
     def test_red_and_anime_builds_is_building
-        status = @ardFeedback.get_status @red_and_blue_anime_xml
+        status = @ardFeedback.get_status xml_jenkins ["red", "blue_anime"]
 
         assert_equal(:building, status)
+    end
+
+
+    def xml_jenkins(colors)
+        xml = "<hudson>"
+
+        colors.each do |color|
+            xml += "<job>"
+            xml += "<name>thats_my_job</name>"
+            xml += "<url>http://localhost:8080/job/thats_my_job/</url>"
+            xml += "<color>#{color}</color>"
+            xml += "</job>"
+        end
+        xml += "</hudson>"
     end
 end
 
