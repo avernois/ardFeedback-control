e3d05ac75cfb99964dfc4eef40f818730aefa886
Antoine <antoine@crafting-labs.fr>
2012-08-16 17:47:51 +0200

    A Test suite to rule them all
    In order to easily launch all test in one command. Useful for travis

---
+++ test/ts_all_tests.rb
@@ -0,0 +1,4 @@
+require_relative "./ard_feedback_args_test.rb"
+require_relative "./ard_feedback_test.rb"
+require_relative "./jenkins_status_parser_test.rb"
+require_relative "./travis_status_parser_test.rb"
