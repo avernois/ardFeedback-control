0af323c528898ddacb5402bd7b285e0f981dc346
Antoine <antoine@crafting-labs.fr>
2012-08-16 17:17:06 +0200

    Add Travis support to command line args
    the url to the api is now given with --url (-u) options
    To specify the use of Jenkins or Travis status, use --jenkins (-j) or --travis (-t). Not both !

--- lib/ard_feedback_args.rb
+++ lib/ard_feedback_args.rb
@@ -2,11 +2,15 @@ require "trollop"
 
 class ArdFeedbackArgs
     def self.parse_args
-        Trollop::options do
-            opt :jenkins, "jenkins api url", :default => "http://localhost:8080/api/xml", :short => "-j"
+        opts = Trollop::options do 
+        	opt :url, "Url to /xml/api for jenkins or builds.json for travis", :type => String, :short => "-u"
+            opt :jenkins, "Use jenkins parser", :short => "-j"
+            opt :travis, "travis api url", :short => "-t"
             opt :serial, "Serial port use to communicate with arduino", :short => "-s", :default => "/dev/ttyACM0"
             opt :refresh, "Delay between requests to jenkins (in seconds)", :short => "-r", :type => Integer, :default => 30
         end
+		Trollop::die :jenkins, "cannot be define with travis" if (opts[:jenkins] && opts[:travis])
 
+		opts
     end
 end
--- test/ard_feedback_args_test.rb
+++ test/ard_feedback_args_test.rb
@@ -2,29 +2,69 @@ require "test/unit"
 require_relative "../lib/ard_feedback_args.rb"
 
 
+
+
 class ArdFeedbackArgsTest < Test::Unit::TestCase
-    def test_should_accept_jenkins_arg
-        ARGV[0] = "--jenkins"
+    def setup
+        #as ARGV is system variable, we flush its content to prevent collision between test
+        ARGV[0] = ""
+        ARGV[1] = ""
+    end
+
+    def test_should_accept_url_arg
+        ARGV[0] = "--url"
         ARGV[1] = "http://my-localhost:8080/api/xml"
+
+        args = ArdFeedbackArgs.parse_args
+        assert_equal("http://my-localhost:8080/api/xml", args[:url])
+    end
+
+    def test_should_accept_short_url_arg
+        ARGV[0] = "-u"
+        ARGV[1] = "http://my-localhost/api/xml"
 
 
         args = ArdFeedbackArgs.parse_args
-        assert_equal("http://my-localhost:8080/api/xml", args[:jenkins])
+        assert_equal("http://my-localhost/api/xml", args[:url])
     end
+
+    def test_should_accept_jenkins_arg
+        ARGV[0] = "--jenkins"
+
+        args = ArdFeedbackArgs.parse_args
+        assert_equal(true, args[:jenkins])
+    end
+
 
     def test_should_accept_short_jenkins_arg
         ARGV[0] = "-j"
-        ARGV[1] = "http://my-localhost:8080/api/xml"
-
 
         args = ArdFeedbackArgs.parse_args
-        assert_equal("http://my-localhost:8080/api/xml", args[:jenkins])
+        assert_equal(true, args[:jenkins])
     end
 
-
-    def test_should_have_default_jenkins_arg
+    def test_jenkins_arg_should_be_false_when_not_define
         args = ArdFeedbackArgs.parse_args
-        assert_equal("http://localhost:8080/api/xml", args[:jenkins])
+        assert_equal(false, args[:jenkins])
+    end
+
+    def test_should_accept_travis_arg
+        ARGV[0] = "--travis"
+
+        args = ArdFeedbackArgs.parse_args
+        assert_equal(true, args[:travis])
+    end
+
+    def test_should_accept_short_travis_arg
+        ARGV[0] = "-t"
+
+        args = ArdFeedbackArgs.parse_args
+        assert_equal(true, args[:travis])
+    end
+
+    def test_travis_arg_should_be_false_when_not_define
+        args = ArdFeedbackArgs.parse_args
+        assert_equal(false, args[:travis])
     end
 
     def test_should_accept_serial_arg
@@ -41,6 +81,14 @@ class ArdFeedbackArgsTest < Test::Unit::TestCase
         
         args = ArdFeedbackArgs.parse_args
         assert_equal("/dev/serial", args[:serial])
+    end
+
+    def test_should_not_accept_travis_and_jenkins
+        ARGV[0] = "-t"
+        ARGV[1] = "-j"
+
+        # well... I don't know how to test that : In that case, Trollop is suppose to die and kill my process. And by consequence kill the process of test... That's not fun
+        #args = ArdFeedbackArgs.parse_args
     end
 
     def test_should_have_default_serial_arg
